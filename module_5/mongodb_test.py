cd10cacfb4865fc090bfd9f5771e253ec25bdde7
Abhishek Upadhyaya <aupadhyaya.osv@fedex.com>
2021-06-20 18:03:17 -0500

    Module 5 - Assignment 5.2 - Add code comments

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,11 +1,16 @@
+# Import pymongo module
 import pymongo
 from pymongo import MongoClient
 
+# Assign mongodb connection string to variable named url
 url = "mongodb+srv://admin:admin@cluster0.murgk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
+# Create MongoClient object
 client = MongoClient(url)
 
+# Connect to database and assign the same to variable named db
 db = client.pytech
 
+# Print list of all collections
 print(db.list_collection_names())
 
